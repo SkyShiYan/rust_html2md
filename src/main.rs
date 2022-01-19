92c892a3998119f26c84f874feba0db7086b3a0b
skyshi <skyshi@tiidian.com>
2022-01-19 21:17:56 +0800

    目初始化-完成基础代码写入

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+fn main() {
+    println!("Hello, world!");
+
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+
+    println!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    println!("Cnverting html to markdown...");
+    let md = html2md::parse_html(&body);
+
+    fs::write(output, md.as_bytes()).unwrap();
+    println!("Converted markdown has been saved in {}", output)
+}
+
